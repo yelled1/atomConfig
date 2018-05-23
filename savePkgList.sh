645d72f8819ab274c6aaeed21ecd948355420e83
S H Yoon <2314710+yelled1@users.noreply.github.com>
2018-05-22 23:54:07 -0400

    Update savePkgList.sh

--- savePkgList.sh
+++ savePkgList.sh
@@ -1,3 +1,4 @@
 apm list --installed --bare > ~/.atom/package.list
 cp /home/hyoon/.emacs.d/init.el .
 cp /home/hyoon/.config/nvim/init.vim .
+#https://discuss.atom.io/t/installed-packages-list-into-single-file/12227/2
